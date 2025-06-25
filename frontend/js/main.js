32c198595af42cfadbff01b02c1f0e1de323afa8
Jericho Leal <199502689+jericholeal@users.noreply.github.com>
2025-06-25 19:06:20 -0400

    chore: move style.css and main.js to separate folders

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,9 +1,8 @@
 // Visitor counter fetch
 
 // API endpoint to fetch visitor count
-
+const = 
 
 // Function to call Lambda function through API Gateway
-
 
 // Call the function to fetch visitor count when the page loads
